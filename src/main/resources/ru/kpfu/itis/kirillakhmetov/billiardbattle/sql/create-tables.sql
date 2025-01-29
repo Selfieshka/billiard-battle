56fec18e13166971a3a1f68452d9873921ce71a5
Kirill Akhmetov <kirillahmetov935@gmail.com>
2025-01-29 13:58:48 +0300

    create database

--- src/main/resources/ru/kpfu/itis/kirillakhmetov/billiardbattle/sql/create-tables.sql
+++ src/main/resources/ru/kpfu/itis/kirillakhmetov/billiardbattle/sql/create-tables.sql
@@ -1,7 +1,15 @@
+CREATE SEQUENCE player_sequence
+    START WITH 100000
+    INCREMENT BY 1
+    CACHE 50;
+
 CREATE TABLE player
 (
-    id       BIGSERIAL PRIMARY KEY NOT NULL,
-    name     VARCHAR(50)           NOT NULL,
-    password VARCHAR(50)           NOT NULL,
-    money    INT                   NOT NULL
+    player_id BIGINT       NOT NULL DEFAULT NEXTVAL('player_sequence'),
+    name      VARCHAR(50)  NOT NULL,
+    password  VARCHAR(255) NOT NULL,
+    money     INT          NOT NULL DEFAULT 1000,
+    ------------------------------
+    CONSTRAINT player_id PRIMARY KEY (player_id),
+    CONSTRAINT name_uq UNIQUE (name)
 );
