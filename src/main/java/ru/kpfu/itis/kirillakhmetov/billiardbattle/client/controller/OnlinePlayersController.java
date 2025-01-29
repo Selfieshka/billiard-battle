ee801ab4768c7ac93f9f93e090dd39861353704e
Kirill Akhmetov <kirillahmetov935@gmail.com>
2025-01-30 00:49:44 +0300

    refactoring online player controller

--- src/main/java/ru/kpfu/itis/kirillakhmetov/billiardbattle/client/controller/OnlinePlayersController.java
+++ src/main/java/ru/kpfu/itis/kirillakhmetov/billiardbattle/client/controller/OnlinePlayersController.java
@@ -3,19 +3,14 @@ package ru.kpfu.itis.kirillakhmetov.billiardbattle.client.controller;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.control.*;
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
-import ru.kpfu.itis.kirillakhmetov.billiardbattle.client.MyApp;
+import ru.kpfu.itis.kirillakhmetov.billiardbattle.client.BilliardBattleApplication;
 import ru.kpfu.itis.kirillakhmetov.billiardbattle.client.scene.GameScene;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class OnlinePlayersController implements Initializable {
-    @FXML
-    Pane parent;
     @FXML
     Button back;
     @FXML
@@ -25,55 +20,31 @@ public class OnlinePlayersController implements Initializable {
     @FXML
     ListView<String> active;
     public static ObservableList<String> strings;
-    double x = 0, y = 0;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        makeDragable();
         back.setOnAction(e -> {
-            MyApp.window.setScene(MyApp.menu);
+            BilliardBattleApplication.window.setScene(BilliardBattleApplication.menu);
         });
+
         active.getStylesheets().add(String.valueOf(getClass().getResource("/css/list-view.css")));
         active.setItems(strings);
         active.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+
         play.setOnAction(e -> {
             if (money.getText().isEmpty() || active.getSelectionModel().getSelectedItem().isEmpty()) {
                 Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                alert.setTitle("Error!");
-                alert.setHeaderText("Please select a opponent and amount of money to bet!!");
+                alert.setTitle("Ошибка");
+                alert.setHeaderText("Выберите соперника и укажите сумму денег");
                 alert.show();
             } else if (GameScene.getPlayer1().getBalance() < Integer.parseInt(money.getText())) {
                 Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                alert.setTitle("Error!");
-                alert.setHeaderText("Not enough money!!");
+                alert.setTitle("Ошибка");
+                alert.setHeaderText("Не хватает денег");
                 alert.show();
             } else {
-                MyApp.outToServer.println("canPlay#" + active.getSelectionModel().getSelectedItem() + "#" + money.getText());
+                BilliardBattleApplication.outToServer.println("canPlay#" + active.getSelectionModel().getSelectedItem() + "#" + money.getText());
             }
         });
-    }
-
-    private void makeDragable() {
-        parent.setOnMousePressed(((event) -> {
-            x = event.getSceneX();
-            y = event.getSceneY();
-        }));
-
-        parent.setOnMouseDragged(((event) -> {
-            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            stage.setX(event.getScreenX() - x);
-            stage.setY(event.getScreenY() - y);
-            stage.setOpacity(0.8f);
-        }));
-
-        parent.setOnDragDone(((event) -> {
-            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            stage.setOpacity(1.0f);
-        }));
-
-        parent.setOnMouseReleased(((event) -> {
-            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            stage.setOpacity(1.0f);
-        }));
     }
 }
